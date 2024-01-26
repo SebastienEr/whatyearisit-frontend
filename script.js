fb772f96168eb71c19a0a83ef7a11c26c0e75af3
Sébastien <pellangeon@gmail.com>
2024-01-26 15:16:34 +0100

    jk

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- fetch('whatyearisit-backend-azure-eta.vercel.app')
+ fetch('https://whatyearisit-backend-azure-eta.vercel.app/year')
 .then(response => response.json())
 .then(data => {
   document.querySelector('#year').textContent = data.year;
